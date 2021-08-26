c76bf71e551d3a06a8710ea990f94b44da73d671
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2021-08-26 23:52:12 +0200

    [UPDATE]

---
+++ Signal/Connections.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HubConnections 
+{
+    public class Connections 
+    {
+        public Dictionary<string, string> currentConnected { get; }
+
+        public void AddConnection( string connectionId, string nickname) {
+            this.currentConnected.Add(connectionId, nickname); // avoid collision
+        }
+
+        public void RemoveConnection(string connectionId) {
+            this.currentConnected.Remove(connectionId);
+        }
+
+        // Looking for connectionId ( not himself )
+        public string findSomeone( string connectionId ) {
+            Dictionary<string, string> dict = this.currentConnected.Where( el => el.Key != connectionId).ToDictionary(el => el.Key, el => el.Value);
+            if ( dict.Count == 0 ) {
+                return "";
+            } else {
+                return dict.First().Key;
+            }
+        }   
+    }
+}
--- Signal/ProfileHub.cs
+++ Signal/ProfileHub.cs
@@ -1,14 +1,51 @@
 using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Threading.Tasks;
+using HubConnections;
 
+// https://docs.microsoft.com/fr-fr/aspnet/signalr/overview/guide-to-the-api/mapping-users-to-connections
 namespace SignalRChat.Hubs
 {
     public class ProfileHub : Hub
     {
+        public Connections connectionsLake = new Connections();
+
+        public readonly string LAKE_ROOM_NAME = "lack";
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.Others.SendAsync("ReceiveMessage", user, message);
         }
+
+        public async Task FindSomeone() {
+            
+        }
+
+        // Call when user click on "start" and make him join the lake to date another ppl connected
+        public async Task JoinLake(string nickname) {
+            this.connectionsLake.AddConnection(Context.ConnectionId, nickname);
+            await Groups.AddToGroupAsync(Context.ConnectionId, LAKE_ROOM_NAME);
+        }
+
+        public async Task LeaveLake() {
+            this.connectionsLake.RemoveConnection(Context.ConnectionId);
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, LAKE_ROOM_NAME);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception) {
+            this.connectionsLake.RemoveConnection(Context.ConnectionId);
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, this.LAKE_ROOM_NAME);
+            Task task = base.OnDisconnectedAsync(exception);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            //string name = Context.User.Identity.Name;
+            //Context.User.
+            //Console.WriteLine("HELLO : " + name);
+            //_connections.Add(name, Context.ConnectionId);
+
+            return base.OnConnectedAsync();
+        }
+
     }
 }
